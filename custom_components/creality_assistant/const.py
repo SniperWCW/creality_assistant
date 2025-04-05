49f01851dcafcd8c256346316d699a2ba46ac08f
Chuffnugget <88729673+Chuffnugget@users.noreply.github.com>
2025-04-05 04:03:24 +0100

    Create const.py

---
+++ custom_components/creality_assistant/const.py
@@ -0,0 +1,12 @@
+DOMAIN = "creality_assistant"
+
+DEFAULT_PORT = 9999
+CONF_IP = "ip"
+CONF_PORT = "port"
+CONF_PASSWORD = "password"
+
+# Key for storing the WebSocket client in hass.data
+DATA_CLIENT = "creality_client"
+
+# Signal used to update sensor entities when new data arrives.
+UPDATE_SIGNAL = "creality_assistant_update"
